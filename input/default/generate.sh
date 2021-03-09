714b10037ec7a32aac5d39f592e4bb7d5d594ffb
Masen Furer <mf@0x26.net>
2021-03-08 22:10:45 -0800

    input/default/generate.py: new python-based generate script
    update generate.sh for 0.3a3 syntax with new output modes and
    dmrconfig template

--- input/default/generate.sh
+++ input/default/generate.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
+# This generates the same codeplug as generate.py
+# using bash scripting. Linux or macOS only.
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 OUTPUT=${OUTPUT:-$DIR/../../OUTPUT}
 python -m dzcb \
     --pnwdigital \
     --seattledmr \
     --default-k7abd \
-    --repeaterbook-state washington oregon \
-    --repeaterbook-proximity-csv "$DIR/prox.csv" \
     --k7abd $DIR/k7abd \
-    --farnsworth-template-json "$DIR/example-md-uv380.json" \
-    --order "$DIR/order.csv" \
-    --exclude "$DIR/exclude.csv" \
-    --replacements "$DIR/replacements.csv" \
+    --repeaterbook-proximity-csv "$DIR/prox.csv" \
+    --repeaterbook-state washington oregon \
     --scanlists-json "$DIR/scanlists.json" \
+    --exclude "$DIR/exclude.csv" \
+    --order "$DIR/order.csv" \
+    --replacements "$DIR/replacements.csv" \
+    --anytone \
+    --dmrconfig "$DIR/example-d878uv.conf" \
+    --farnsworth-template-json "$DIR/example-md-uv380.json" \
+    --gb3gf \
 -- "$OUTPUT/$(basename "$DIR")"
