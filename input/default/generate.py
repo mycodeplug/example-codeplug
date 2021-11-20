84e9ad35c9c8c02ec9b510fc967135087fd85c13
mkaylor <mike@mkaylor.com>
2021-11-19 18:40:39 -0600

    Test 4
    Removing all but Texas

--- input/default/generate.py
+++ input/default/generate.py
@@ -12,12 +12,12 @@ cp_dir = Path(__file__).parent
 output = Path(os.environ.get("OUTPUT") or (cp_dir / ".." / ".." / "OUTPUT"))
 
 CodeplugRecipe(
-    source_pnwdigital=True,
-    source_seattledmr=True,
-    source_default_k7abd=True,
+    source_pnwdigital=False,
+    source_seattledmr=False,
+    source_default_k7abd=False,
     source_k7abd=[(cp_dir / "k7abd")],
     source_repeaterbook_proximity=cp_dir / "prox.csv",
-    repeaterbook_states=["washington", "oregon"],
+    repeaterbook_states=["Texas"],
     repeaterbook_name_format='{Callsign} {Nearest City} {Landmark}',
     scanlists_json=cp_dir / "scanlists.json",
     exclude=cp_dir / "exclude.csv",
@@ -26,5 +26,5 @@ CodeplugRecipe(
     output_anytone=True,
     output_dmrconfig=[(cp_dir / "example-d878uv.conf")],
     output_farnsworth=[(cp_dir / "example-md-uv380.json")],
-    output_gb3gf=True
+    output_gb3gf=False
 ).generate(output / cp_dir.name)
